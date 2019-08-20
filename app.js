0aee63af98aa8ddf071eb06dac259bb2de293ba3
HellooCK <46767604+HellooCK@users.noreply.github.com>
2019-08-20 12:54:41 +1000

    Add image gallery section with javascript effect

--- app.js
+++ app.js
@@ -1,40 +1,67 @@
-//! My version
+//! Navbar Animation
+
+// My version
+
+function navBorder() {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav a");
+  const header = document.querySelector("header");
+
+  sections.forEach(section => {
+    section.addEventListener("mouseenter", e => {
+      let id = e.target.id;
+
+      navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${id}`) {
+          link.classList.add("active");
+        }
+      });
+    });
+  });
+
+  header.addEventListener("mouseenter", function() {
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+    });
+  });
+}
+
+// navBorder();
+
+// Tutorial
 
 // function navBorder() {
 //   const sections = document.querySelectorAll("section");
 //   const navLinks = document.querySelectorAll("nav a");
 
 //   sections.forEach(section => {
-//     section.addEventListener("mouseenter", e => {
-//       let id = e.target.id;
-
-//       navLinks.forEach(link => {
-//         if (link.getAttribute("href") === `#${id}`) {
-//           console.log(link);
-//           link.classList.add("active");
-//         }
-//       });
+//     section.addEventListener("mouseenter", function() {
+//       const id = this.getAttribute("id");
+//       const navActive = document.querySelector(`a[href="#${id}"]`);
+//       navLinks.forEach(link => link.classList.remove("active"));
+//       navActive.classList.add("active");
+//       console.log(navActive);
 //     });
 //   });
 // }
 
-// navBorder();
+navBorder();
 
-//! Tutorial
+//! Image Gallery
 
-function navBorder() {
-  const sections = document.querySelectorAll("section");
-  const navLinks = document.querySelectorAll("nav a");
+function imageGallery() {
+  const previews = document.querySelectorAll(".room-preview img");
+  const highlight = document.querySelector(".room-highlight");
 
-  sections.forEach(section => {
-    section.addEventListener("mouseenter", function() {
-      const id = this.getAttribute("id");
-      const navActive = document.querySelector(`a[href="#${id}"]`);
-      navLinks.forEach(link => link.classList.remove("active"));
-      navActive.classList.add("active");
-      console.log(navActive);
+  previews.forEach(preview => {
+    preview.addEventListener("click", e => {
+      highlight.src = e.target.getAttribute("src");
+      previews.forEach(preview => preview.classList.remove("room-active"));
+      preview.classList.add("room-active");
+      console.log(e.target.getAttribute("src"));
     });
   });
 }
 
-navBorder();
+imageGallery();
